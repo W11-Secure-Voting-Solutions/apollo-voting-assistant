637c7b1ff75584d2a71c14c1deefb0a64183c5ff
piotrek <piotr.w.kawa@gmail.com>
2020-01-07 20:38:03 +0100

    Add axios, send a GET request to specific URL

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,7 +1,13 @@
 const axios = require("axios");
-const URL = "";
 
-async function queryBB() {
-    const response = await axios.get(URL);
-    return response;
+const axiosInstance = axios;
+
+async function get(url) {
+    return await axiosInstance.get(url);
 }
+
+async function post(url, body) {
+    return await axiosInstance.post(url, body);
+}
+
+export { get, post }
