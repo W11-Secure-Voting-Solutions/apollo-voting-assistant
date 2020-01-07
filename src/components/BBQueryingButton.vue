f515993ba6b51e10359694c0f709ef36e2097d39
piotrek <piotr.w.kawa@gmail.com>
2020-01-07 21:35:46 +0100

    Add initial work on Vuex integration

--- src/components/BBQueryingButton.vue
+++ src/components/BBQueryingButton.vue
@@ -1,21 +1,25 @@
 <template>
-    <button @click="queryBB">Query Bulletin Board</button>
+  <button @click="queryBulletinBoard">Query Bulletin Board</button>
 </template>
 
 <script>
-import { get }  from '../services/httpService.js';
+import { get } from "../services/httpService.js";
+import store  from "../store/store";
 
 export default {
-    methods: {
-        async queryBB() {
-            const URL = "https://reqres.in/api/users/2"; // TODO: move to state
-            const response = await get(URL);
-            // TODO: add to the state and display it
-        }
+  data() {
+    return {
+      bulletinBoardURL: "https://reqres.in/api/users/2"//this.$store.getters.qrCode
     }
-}
+  },
+  methods: {
+    async queryBulletinBoard() {
+      const response = await get(this.bulletinBoardURL);
+      console.log(`Response ${JSON.stringify(response)}`)
+      store.dispatch("setBulletinBoardContent", [response]);
+      console.log(this.$store.getters.bulletinBoardContent);
+      // TODO: add to the state and display it
+    }
+  }
+};
 </script>
-
-<style scoped>
-
-</style>
