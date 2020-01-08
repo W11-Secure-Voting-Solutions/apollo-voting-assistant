f06227457894f4130e5bb9174027ec92fcdb9a65
Fadion96 <nowak196@gmail.com>
2020-01-08 19:44:34 +0100

    Add Bulletin Board content display

---
+++ src/components/BulletinBoard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <b> {{ bulletinBoard }} </b>
+    </div>
+</template>
+
+<script>
+import store from "../store/store";
+
+export default {
+    name: "BulletinBoard",
+    computed: {
+        bulletinBoard() {
+            let contentBB = this.$store.getters.bulletinBoardContent;
+            if (contentBB === undefined || contentBB === "") {
+                contentBB = "No Bulletin Board Yet!";
+            }
+            return contentBB;
+        }
+    }
+    
+}
+</script>
