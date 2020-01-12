f9f91c90881e30f505ab843e7638810f72c8dc66
piotrek <piotr.w.kawa@gmail.com>
2020-01-12 23:32:19 +0100

    Delete unused imports

--- src/components/BulletinBoardDashboard.vue
+++ src/components/BulletinBoardDashboard.vue
@@ -5,8 +5,6 @@
 </template>
 
 <script>
-import { get } from "../services/httpService.js";
-import store from "../store/store";
 import { Vue, Component } from "vue-property-decorator";
 import BulletinBoard from "./BulletinBoard";
 
