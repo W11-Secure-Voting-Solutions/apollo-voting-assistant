f6edfab95f57bf1d3d858d877a52ef3df0c2e49c
Łukasz Klekowski <34490533+lukaszklekowski@users.noreply.github.com>
2020-01-14 10:13:11 +0100

    Feature/favicon (#12)
    * Add /public directory
    
    * add new favicon
    
    * change favicon and delete routers
    
    Co-authored-by: Piotr Kawa <piotr.w.kawa@gmail.com>

--- src/main.js
+++ src/main.js
@@ -3,12 +3,10 @@ import App from './App.vue'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import store from './store/store';
-import router from './routes/routes';
 
 Vue.config.productionTip = false;
 Vue.use(Element);
 new Vue({
   render: h => h(App),
-  store,
-  router
+  store
 }).$mount('#app');
