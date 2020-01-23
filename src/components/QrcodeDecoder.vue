d114f8e699e6fddd7d479bfe8a15dce71a027103
Łukasz Klekowski <34490533+lukaszklekowski@users.noreply.github.com>
2020-01-23 16:59:02 +0100

    Fix QR code validation (#18)

--- src/components/QrcodeDecoder.vue
+++ src/components/QrcodeDecoder.vue
@@ -31,23 +31,21 @@ export default class QrcodeDecoder extends Vue {
   async onDecode(result) {
     console.log("[QR Code] New QR code decoded.");
     result = JSON.parse(result);
-
-    if (this.validateQrCode(result)) {
-      const sessionId = result.session_id;
-      const kRand = result.k_rand;
+    if (this.validateQRCode(result)) {
+      const sessionId = result.sessionId;
+      const kRand = result.kRand;
       this.setSessionId(sessionId);
       this.setKRand(kRand);
-      result = `session_id: ${sessionId}, k_rand: ${kRand}`
+      result = `Session ID: ${sessionId}`
     } else {
-      result = "Qr Code is invalid";
+      result = "QR Code is invalid";
     }
     this.setQrCode(result);
     console.log("[QR Code] QR code saved in a state.");
   }
   
-  validateQrCode(qrCodeContent) {
-    const condition = qrCodeContent.session_id && qrCodeContent.k_rand;
-    return condition;
+  validateQRCode(qrCodeContent) {
+    return qrCodeContent.sessionId && qrCodeContent.kRand;
   }
 }
 </script>
